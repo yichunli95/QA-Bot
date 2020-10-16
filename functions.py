d2fb5e7920cdcbb3298724f2747fe4674a2ef4f3
Sanctimony <qym925998054@qq.com>
2020-10-15 22:27:50 -0400

    basic function

--- functions.py
+++ functions.py
@@ -2,12 +2,13 @@
 import io
 import nltk
 import re
+import spacy
 
 
 def open_document(file_path):
     with io.open(file_path, 'r', encoding='utf8') as f:
         document = f.read()
-    document = re.sub('[\n]+', '. ', document)
+    document = re.sub('[\n]+', ' . ', document)
     return document
 
 
@@ -15,20 +16,26 @@ def tokenize_sentence(document):
     return nltk.sent_tokenize(document)
 
 
-def tokenize_word(sentence):
-    return nltk.word_tokenize(sentence)
+def tokenize_word(nlp, sent):
+    sentence = nlp(sent)
+    return [token for token in sentence]
 
 
-def pos_tag(token):
-    # token: list of words in a sentence
-    return nltk.pos_tag(token)
+def pos_tag(nlp, sent):
+    sentence = nlp(sent)
+    return [token.pos_ for token in sentence]
 
 
 # if __name__ == '__main__':
-#     # download tagger information
-#     # nltk.download('averaged_perceptron_tagger')
+#     nlp = spacy.load('en_core_web_sm')
 #     document = open_document("../noun_counting_data/a1.txt")
 #     sentences = tokenize_sentence(document)
-#     tokens = [tokenize_word(sentence) for sentence in sentences]
-#     pos_tags = [pos_tag(token) for token in tokens]
-
+#     for sent in sentences:
+#         print(tokenize_word(nlp, sent))
+#         print(pos_tag(nlp, sent))
+#         sentence = nlp(sent)
+#         # noun chunks
+#         print([nc for nc in sentence.noun_chunks])
+#
+#         # NER
+#         print([(ent.text, ent.label_) for ent in sentence.ents])
